a6fe888f9cf2514107894df305ec6af11c0b9b27
Lucio Felisaz <luciof.fields@gmail.com>
2025-04-29 16:42:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = Math.floor(Math.random() * 5);
+
+console.log("Citations: " + [a]);
+
+let citations = ["On ne se baigne jamais deux fois dans le meme fleuve <br/>", "Je sais que je ne sais rien <br/>", "L'homme est un animal politique <br/>", "La mort n'est rien pour nous <br/>", "Je pense, donc je suis <br/>"];
+document.write(citations[a]);
+
+let auteurs = ["-Heraclite-", "-Socrate-", "-Aristote-", "-Epicure-", "-Renee Descartes-"];
+document.write(auteurs[a]);
+
